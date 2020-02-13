60120a3a1ca45f0bd74d3b5231ace14c6de83c42
tmquangbkhn <quangtm.bkhn@gmail.com>
2020-02-13 17:23:41 +0700

    add schema

---
+++ src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+let Schema = mongoose.Schema;
+let UserSchema =new Schema({
+	username: String,
+	gender: {type: String,default:male},
+	phone:{type:Number,default:null},
+	address:{type:String,default:null},
+	avatar:{type:String,default:"avatar-default.jpg"},
+	role:{type:String,default:"user"},
+	local:{
+		email:{type:String,strim:true},
+		password:String,
+		isActive:{type:Boolean, default:false},
+		verifyToken:String
+	},
+	facebook:{
+		uid: String,
+		token:String,
+		email:{type:String,strim:true}
+	},
+	google:{
+		uid: String,
+		token:String,
+		email:{type:String,strim:true}
+	},
+	createdAt:{type:Number,default:Date.now},
+	updatedAt:{type:Number,default:null},
+	deletedAt:{type:Number,default:null}
+});
+
+module.exports =mongoose.model("user",UserSchema);
