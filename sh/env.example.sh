d684cdae46bef1ab90180b67691e1d2ede43978b
tmquangbkhn <quangtm.bkhn@gmail.com>
2020-02-15 01:17:39 +0700

    connect mongodb

---
+++ sh/env.example.sh
@@ -0,0 +1,8 @@
+# config enviroment variable 
+export DB_CONNECTION = "mongodb";
+export DB_HOST = "localhost";
+export DB_PORT = "27017";
+export DB_NAME = "messenger";
+export DB_USERNAME = "";
+export DB_PASSWORD = "";
+
